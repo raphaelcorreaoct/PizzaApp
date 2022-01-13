ec44da34d036565fbfee5960cd9eb3e85a1d98c9
Raphael Correa <raphael.correa.b@gmail.com>
2022-01-13 00:15:43 -0300

    Add fonts

--- App.js
+++ App.js
@@ -5,30 +5,10 @@ import {Box} from './components';
 import {ThemeProvider} from 'styled-components';
 import theme from './components/Theme';
 
-import {
-  useFonts,
-  Nunito_300Light,
-  Nunito_400Regular,
-  Nunito_700Bold,
-} from '@expo-google-fonts/nunito';
-import {ActivityIndicator} from 'react-native';
-
 const App = () => {
-  let [fontsLoaded] = useFonts({
-    Nunito_300Light,
-    Nunito_400Regular,
-    Nunito_700Bold,
-  });
-
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? (
-        <Box flex={1} alignItem="center" justifyContent="center">
-          <ActivityIndicator />
-        </Box>
-      ) : (
-        <Details />
-      )}
+      <Details />
     </ThemeProvider>
   );
 };
--- screens/Details.js
+++ screens/Details.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+
 import Box from './../components/Box';
 import {Txt} from './../components/Txt';
 
 const Details = () => {
   return (
     <Box>
-      <Txt> Esse é um teste</Txt>
+      <Txt>Esse é um teste</Txt>
     </Box>
   );
 };
